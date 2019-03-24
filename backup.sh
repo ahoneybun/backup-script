805f9f6f0ff0a5ea345ab98435c543d5d1df28d6
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-03-23 23:31:13 -0600

    update style

--- backup.sh
+++ backup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # Intro
 # ------
-echo "------"
+echo "---------------------------------------------"
 echo "Welcome to version 0.1 of the Backup Script"
-echo "------"
+echo "---------------------------------------------"
 sleep 2
 clear
 
 read -p 'Username: ' uservar
 
 sudo mount /dev/sda1 /media/$uservar/ExtraDrive1
-echo "------"
+echo "---------------------------"
 echo "Mounting your ExtraDrive1"
-echo "------"
+echo "---------------------------"
 sleep 2
 clear
 
 echo "Hello $uservar what would you like to backup today?"
-echo "------"
+echo "---------------"
 echo "[1] Pictures"
 echo "[2] Documents"
 echo "[3] Everything"
-echo "------"
+echo "---------------"
 echo -n "Enter choice: "; read choice
 case "$choice" in
 1) cp -ru /home/$uservar/Pictures/* /media/$uservar/ExtraDrive1/Backups/Pictures/
@@ -32,8 +32,10 @@ case "$choice" in
    cp -ru /home/$uservar/Documents/* /media/$uservar/ExtraDrive1/Backups/Documents/
    ;;
 esac
+echo "---------------------------------"
 echo "Done backing up those dictories"
 echo "Have a great day!"
+echo "---------------------------------"
 sleep 2
 clear
 
