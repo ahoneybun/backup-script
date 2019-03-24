08158a8c44a7486d3a56bd87b6505af9c46126be
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-03-23 20:03:02 -0600

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Intro
+# ------
+echo "------"
+echo "Welcome to version 0.1 of the Backup Script"
+echo "------"
+sleep 2
+clear
+
+read -p 'Username: ' uservar
+
+echo "Hello $uservar what would you like to backup today?"
+echo "------"
+echo "[1] Pictures"
+echo "[2] Documents"
+echo "[3] Everything"
+echo "------"
+echo -n "Enter choice: "; read choice
+case "$choice" in
+1) cp -r /home/$uservar/Pictures/* /media/$uservar/ExtraDrive1/Backups/Pictures/
+   ;;
+2) cp -r /home/$uservar/Documents/* /media/$uservar/ExtraDrive1/Backups/Documents/
+   ;;
+3) cp -r /home/$uservar/Pictures/* /media/$uservar/ExtraDrive1/Backups/Pictures/ && cp -r /home/$uservar/Documents/* /media/$uservar/ExtraDrive1/Backups/Documents/
+   ;;
+esac
