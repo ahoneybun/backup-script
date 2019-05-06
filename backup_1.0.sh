995af912b44effb6514a0177c45d68f9b8db6b54
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-05-05 19:31:21 -0600

    add archive method test

--- backup_1.0.sh
+++ backup_1.0.sh
@@ -30,6 +30,7 @@ echo "[1] Pictures"
 echo "[2] Documents"
 echo "[3] Videos"
 echo "[4] Everything"
+echo "[5] WIP Archive Method"
 echo "---------------"
 echo -n "Enter choice: "; read choice
 case "$choice" in
@@ -42,12 +43,20 @@ case "$choice" in
 4) cp -ru /home/$uservar/Pictures/* /media/$uservar/Steam-Library/Backups/Pictures/ 
    cp -ru /home/$uservar/Documents/* /media/$uservar/Steam-Library/Backups/Documents/
    cp -ru /home/$uservar/Videos/* /media/$uservar/Steam-Library/Backups/Videos
+   cp -ru /home/$uservar/android-sdk-linux/* /media/$uservar/Steam-Library/Backups/android-sdk-linux/
    cp -ru /home/$uservar/.bashrc /media/$uservar/Steam-Library/Backups/
+   scp /home/$uservar/.bashrc root@vault.ahoneybun.net:/root/Backup/
    cp -ru /home/$uservar/.ssh/id_rsa* /media/$uservar/Steam-Library/Backups/.ssh/
    cp -ru /home/$uservar/.config/i3 /media/$uservar/Steam-Library/Backups/.config/i3/
    cp -ru /home/$uservar/.config/i3status /media/$uservar/Steam-Library/Backups/.config/i3status/
    cp -ru /home/$uservar/.config/sway/* /media/$uservar/Steam-Library/Backups/.config/sway/
    ;;
+5) tar -czvf Pictures.tar.gz /home/$uservar/Pictures/* 
+   mv Pictures.tar.gz /media/$uservar/Steam-Library/Backups/
+   tar -czvf Documents.tar.gz /home/$uservar/Documents/* 
+   mv Documents.tar.gz /media/$uservar/Steam-Library/Backups/
+   tar -czvf config-files.tar.gz /home/$uservar/.config/* 
+   mv config-files.tar.gz /media/$uservar/Steam-Library/Backups/
 esac
 clear
 
