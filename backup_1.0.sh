109142fa2f12ab6fb6834966e9300c9b4527c123
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-09-18 16:36:10 -0600

    merge changes from the laptop-galp3 branch

---
+++ backup_1.0.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Dead simple backup script written in Bash 
+# By Aaron Honeycutt
+---
+
+# Defining some variables
+day=$(date +%Y-%m-%d)
+
+clear
+echo "---------------------------------------------"
+echo "Welcome to version 1.0 of the Backup Script"
+echo "---------------------------------------------"
+sleep 2
+clear
+
+# Selecting which dictories to backup
+echo "-----------------------------------------------------------"
+echo "Hello $USER how would you like to backup today?"
+echo "-----------------------------------------------------------"
+echo "[1] Archive Method"
+echo "-------------------"
+echo -n "Enter choice: "; read choice
+case "$choice" in
+1) mkdir /media/$USER/Backup/$day/
+   tar -czvf Pictures-$day.tar.gz ~/Pictures/* 
+   mv Pictures-$day.tar.gz /media/$USER/Backup/$day/
+   tar -czvf Documents-$day.tar.gz ~/Documents/* 
+   mv Documents-$day.tar.gz /media/$USER/Backup/$day/
+   mv ~/Backup-Drive/Documents-$day.tar.gz /media/$USER/Backup/$day/
+   tar -czvf config-files-$day.tar.gz ~/.config/* 
+   mv config-files-$day.tar.gz /media/$USER/Backup/$day/
+esac
+
+# Exit script
+echo "---------------------------------"
+echo "Done backing up those dictories"
+echo "Have a great day!"
+echo "---------------------------------"
+sleep 2
