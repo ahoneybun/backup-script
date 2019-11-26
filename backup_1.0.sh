ef5d770fb05b46c5149c9470999ff502a230c925
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-11-25 19:38:14 -0700

    fix typos, switch to only rsync and add scp backup

--- backup_1.0.sh
+++ backup_1.0.sh
@@ -3,9 +3,6 @@
 # By Aaron Honeycutt
 ---
 
-# Defining some variables
-day=$(date +%Y-%m-%d)
-
 clear
 echo "---------------------------------------------"
 echo "Welcome to version 1.0 of the Backup Script"
@@ -17,29 +14,24 @@ clear
 echo "-----------------------------------------------------------"
 echo "Hello $USER how would you like to backup today?"
 echo "-----------------------------------------------------------"
-echo "[1] Archive Method"
-echo "[2] rsync method"
+echo "[1] rsync Method"
 echo "-------------------"
 echo -n "Enter choice: "; read choice
 case "$choice" in
-1) mkdir /media/$USER/Backup/$day/
-   tar -czvf Pictures-$day.tar.gz ~/Pictures/* 
-   mv Pictures-$day.tar.gz /media/$USER/Backup/$day/
-   tar -czvf Documents-$day.tar.gz ~/Documents/* 
-   mv Documents-$day.tar.gz /media/$USER/Backup/$day/
-   mv ~/Backup-Drive/Documents-$day.tar.gz /media/$USER/Backup/$day/
-   tar -czvf config-files-$day.tar.gz ~/.config/* 
-   mv config-files-$day.tar.gz /media/$USER/Backup/$day/
-   ;;
-2) rsync -av --progress ~/Pictures/ /media/$USER/Backup/Pictures/
+1) rsync -av --progress ~/Pictures/ /media/$USER/Backup/Pictures/
    rsync -av --progress ~/Documents/ /media/$USER/Backup/Documents/
    rsync -av --progress ~/.config/ /media/$USER/Backup/.config/
+   rsync -av --progress ~/.bashrc /media/$USER/Backup/
+   rsync -av --progress ~/.ssh/id_rsa* /media/$USER/Backup/.ssh/
 
+   scp ~/.ssh/id_rsa* root@vault.ahoneybun.net:/root/Backups/.ssh/
+   scp ~/.bashrc root@vault.ahoneybun.net:/root/Backups/
 esac
 
 # Exit script
 echo "---------------------------------"
-echo "Done backing up those dictories"
+echo "Done backing up those directories"
+echo "and files"
 echo "Have a great day!"
 echo "---------------------------------"
 sleep 2
