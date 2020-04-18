7647039e88a76fb805e0b8131c1adcf4d8c274e7
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2020-04-17 18:16:27 -0600

    complete remove snapshot feature and everything exits correctly

--- backup.sh
+++ backup.sh
@@ -10,26 +10,34 @@ echo "------------------------------------------------------"
 echo "[1] Backup Pictures, Documents, Videos directories"
 echo "[2] Backup all of your home directory"
 echo "[3] View snapshots"
+echo "[4] Remove snapshots"
 
 echo "------------------------------------------------------"
 echo -n "Enter choice: "; read choice
 case "$choice" in
 
 1) sudo restic -r /media/aaronh/Backup/ backup ~/Music/ ~/Documents/ ~/Pictures/ ~/Videos/
+   exit 1
    ;; 
 2) sudo restic -r /media/aaronh/Backup/ backup ~/.
+   exit 1
    ;;
 3) sudo restic -r /media/aaronh/Backup/ snapshots
+   exit 1
+   ;;
+4) echo "Displaying snapshots" 
+   sudo restic -r /media/aaronh/Backup/ snapshots
+   ;;
 esac
+
+if [ "$choice" -ne 4 ]; then
+   clear
+fi
 
 echo "------------------------------------------------------"
 echo "Which snapshot do you want to remove?"
 echo "------------------------------------------------------"
 
-echo -n "Enter choice: "; read choice2
-case "$choice2" in 
+read id
 
-$id) echo "Removing ID $id"
 sudo restic -r /media/aaronh/Backup/ forget $id
-   ;;
-esac
