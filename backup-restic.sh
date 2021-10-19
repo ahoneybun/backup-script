5da7ec15b7cf318edfa6c1fa5dc1d0f36f957e76
Aaron Honeycutt <aaron@system76.com>
2021-10-18 19:51:13 -0600

    creating and viewing snapshots work

--- backup-restic.sh
+++ backup-restic.sh
@@ -13,52 +13,54 @@ echo ""
 
 echo "[0] Create SFTP backup"
 echo "[1] Create local backup"
-echo "[3] View snapshots"
-echo "[4] Remove snapshots"
+echo "[2] Manage SFTP repository"
+echo "[3] Manage local repostory"
 
-echo "------------------------------------------------------"
-echo -n "Enter choice: "; read choice
-case "$choice" in
-
-0) echo ""
+server_repo () {
+   echo ""
    echo "What is the user@hostname for the server?"
    read serverInfo
    echo ""
    echo "What is the name of the folder?"
    read serverFolder
    echo ""
+}
+
+local_repo () {
+   echo ""
+   echo "What is the destination of the repo?"
+   read localInfo
+   echo ""
+}
+
+echo "------------------------------------------------------"
+echo -n "Enter choice: "; read choice
+case "$choice" in
+
+0) server_repo
    restic -r sftp:$serverInfo:$serverFolder init
    echo ""
    echo "What folders would you like to backup?"
    read serverSource
    echo ""
    restic -r sftp:$serverInfo:$serverFolder --verbose backup $serverSource
-   exit 1
    ;;
-1) echo ""
-   echo "What is the destination of the repo?"
-   read localInfo
-   echo ""
+
+1) local_repo
    restic init --repo $localInfo
    echo ""
    echo "What folders would you like to backup?"
    read localSource
    echo ""
    restic -r $localInfo --verbose backup $localSource
-   exit 1
    ;;
-3) echo ""
-   echo "What is the user@hostname for the server?"
-   read serverInfo
-   echo ""
-   echo "What is the name of the folder?"
-   read serverFolder
-   echo ""
+
+2) server_repo
    restic -r sftp:$serverInfo:$serverFolder snapshots
-   exit 1
    ;;
-4) echo "Displaying snapshots" 
-   sudo restic -r /media/aaronh/ExtraDrive/Backup/ snapshots
+
+3) local_repo
+   restic -r $localInfo snapshots
    ;;
 
 esac
